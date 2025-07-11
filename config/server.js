367d36cb78f45a2d516b9f252caf35a2c0642952
algazahp <algazahp19@gmail.com>
2025-07-11 17:03:23 +0700

    fixfinalharusnyafix

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
   app: {
-    keys: env.array('abc123','def456','ghi789','jkl012'),
+    keys: env.array('APP_KEYS', ['abc123','def456','ghi789','jkl012']),
   },
 });
